d6860343092bb1e7cfd2268e27f5d8bfd24aa796
Yanottama Oktabrian <oktatama4@gmail.com>
2024-06-03 13:58:36 +0700

    feat: update informasi pelatihan

--- data-penempatan.php
+++ data-penempatan.php
@@ -21,8 +21,7 @@
         <thead>
           <tr class="table-primary">
             <th scope="col">No.</th>
-            <th scope="col">Angkatan</th>
-            <th scope="col">Jumlah Siswa Pelatihan</th>
+            <th scope="col">Bulan</th>
             <th scope="col">Jumlah Penempatan Wirausaha</th>
             <th scope="col">Jumlah Penempatan Industri</th>
             <th scope="col">Ket. APBN / APBD</th>
@@ -31,10 +30,30 @@
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>1</td>
-            <td>96</td>
+            <td>April</td>
+            <td>Laki-laki: 0<br />Perempuan: 0</td>
+            <td>Laki-laki: 0<br />Perempuan: 0</td>
+            <td>APBN</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>April</td>
             <td>Laki-laki: 2<br />Perempuan: 1</td>
             <td>Laki-laki: 11<br />Perempuan: 2</td>
+            <td>APBD</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Mei</td>
+            <td>Laki-laki: 3<br />Perempuan: 5</td>
+            <td>Laki-laki: 20<br />Perempuan: 17</td>
+            <td>APBN</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Mei</td>
+            <td>Laki-laki: 12<br />Perempuan: 9</td>
+            <td>Laki-laki: 32<br />Perempuan: 11</td>
             <td>APBD</td>
           </tr>
         </tbody>
