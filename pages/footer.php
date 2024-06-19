7d25e8374d2fccd8cb80efb9255e7c95b9423b95
Yanottama Oktabrian <oktatama4@gmail.com>
2024-06-19 12:21:44 +0700

    feat: update informasi footer

--- pages/footer.php
+++ pages/footer.php
@@ -4,68 +4,89 @@
     <div class="d-flex flex-row">
       <div class="col-6">
         <h5>UPT BLK Singosari</h5>
-        <div class="d-flex flex-row">
-          <svg class="bi" width="24" height="24">
-            <use xlink:href="#location" />
-          </svg>
-          <p class="ms-3">
-            Jl. Raya Singosari, Song Song, Ardimulyo, Kec. Singosari, Kabupaten
-            Malang, Jawa Timur, 65153
-            <a target="_blank" href="https://maps.app.goo.gl/LLyo37feniFJM5fr6">Maps</a>
-          </p>
+        <div class="d-flex flex-column">
+
+          <div class="d-flex flex-row align-items-center">
+            <svg class="bi" width="24" height="24">
+              <use xlink:href="#location" />
+            </svg>
+            <div class="d-flex flex-column">
+              <p class="ms-3 mb-0">Alamat :</p>
+              <p class="ms-3 mb-0">Jl. Raya Singosari, Song Song, Ardimulyo, Kec. Singosari, Kabupaten
+                Malang, Jawa Timur, 65153</p>
+              <p class="ms-3">
+                <a target="_blank" href="https://maps.app.goo.gl/LLyo37feniFJM5fr6">Buka GMaps</a>
+              </p>
+            </div>
+          </div>
+          <div class="d-flex flex-row align-items-center">
+            <svg class="bi m-0" width="24" height="24">
+              <use xlink:href="#email" />
+            </svg>
+            <div class="d-flex flex-column">
+              <p class="ms-3 mb-0">Email :</p>
+              <p class="ms-3 mb-0">blk.singosari.jatim@gmail.com</p>
+              <p class="ms-3 mb-0">
+                <a target="_blank" href="mailto:blk.singosari.jatim@gmail.com?cc=&bcc=&subject=Informasi%20Pelatihan&body=Halo%20Admin%20UPT%20BLK%20Singosari%2C%0ASelamat%20pagi%2C%20share%20informasi%20pelatihan%20dong...%0ATerima%20kasiihhhh....">
+                  Email kami!
+                </a>
+              </p>
+            </div>
+            <div class="d-flex flex-row align-items-center">
+              <svg class="bi m-0" width="24" height="24">
+                <use xlink:href="#telephone" />
+              </svg>
+              <div class="d-flex flex-column">
+                <p class="ms-3 mb-0">Telepon Kantor :</p>
+                <p class="ms-3 m-0">0341 - 458055</p>
+                <p class="ms-3 m-0">Whatsapp</p>
+                <p class="ms-3 m-0">082334352225</p>
+                <p class="ms-3">
+                  <a target="_blank" href="https://wa.me/6282334352225?text=Halo%20min,%20mau%20nanya%20nih!">
+                    Hubungi kami!
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="d-flex flex-row align-items-center">
-          <svg class="bi m-0" width="24" height="24">
-            <use xlink:href="#telephone" />
-          </svg>
-          <div class="">
-            <p class="ms-3 m-0">0341 - 458055</p>
-            <p class="ms-3">
-              082334352225
-              <a target="_blank" href="https://wa.me/6282334352225?text=Halo%20min,%20mau%20nanya%20nih!">
-                Hubungi kami!
+
+        <div class="justify-content-end col-4 offset-1">
+          <form>
+            <h5>Subscribe Rek!</h5>
+            <p>Ikuti untuk informasi lowongan, pelatihan, dan berita kami.</p>
+            <div class="d-flex w-100 gap-2">
+              <label for="newsletter1" class="visually-hidden">Alamat email</label>
+              <input id="newsletter1" type="text" class="form-control" placeholder="Alamat email" />
+              <button class="btn btn-primary" type="button">Subscribe</button>
+            </div>
+          </form>
+          <ul class="mt-3 mx-0 list-unstyled d-flex">
+            <li>
+              <a class="link-dark" href="https://www.youtube.com/@blk_singosari"><svg class="bi" width="24" height="24">
+                  <use xlink:href="#youtube" />
+                </svg>
               </a>
-            </p>
-          </div>
+            </li>
+            <li class="ms-3">
+              <a class="link-dark" href="https://www.instagram.com/blk_singosari/"><svg class="bi" width="24" height="24">
+                  <use xlink:href="#instagram" />
+                </svg>
+              </a>
+            </li>
+            <li class="ms-3">
+              <a class="link-dark" href="https://www.facebook.com/uptpksingosari/"><svg class="bi" width="24" height="24">
+                  <use xlink:href="#facebook" />
+                </svg>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
 
-      <div class="justify-content-end col-4 offset-1">
-        <form>
-          <h5>Subscribe Rek!</h5>
-          <p>Ikuti untuk informasi lowongan, pelatihan, dan berita kami.</p>
-          <div class="d-flex w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden">Alamat email</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Alamat email" />
-            <button class="btn btn-primary" type="button">Subscribe</button>
-          </div>
-        </form>
-        <ul class="mt-3 mx-0 list-unstyled d-flex">
-          <li>
-            <a class="link-dark" href="https://www.youtube.com/@blk_singosari"><svg class="bi" width="24" height="24">
-                <use xlink:href="#youtube" />
-              </svg>
-            </a>
-          </li>
-          <li class="ms-3">
-            <a class="link-dark" href="https://www.instagram.com/blk_singosari/"><svg class="bi" width="24" height="24">
-                <use xlink:href="#instagram" />
-              </svg>
-            </a>
-          </li>
-          <li class="ms-3">
-            <a class="link-dark" href="https://www.facebook.com/uptpksingosari/"><svg class="bi" width="24" height="24">
-                <use xlink:href="#facebook" />
-              </svg>
-            </a>
-          </li>
-        </ul>
+      <div class="d-flex justify-content-between py-4 my-4 border-top">
+        <p>&copy; 2022 - 2024 UPT BLK Singosari.</p>
       </div>
-    </div>
-
-    <div class="d-flex justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2022 - 2024 UPT BLK Singosari.</p>
-    </div>
   </footer>
 </div>
 
