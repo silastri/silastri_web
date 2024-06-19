751fbf28447b30e96265432f847fafadef536fdf
Yanottama Oktabrian <oktatama4@gmail.com>
2024-06-19 12:54:43 +0700

    feat: ubah tampilan footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,50 +1,49 @@
 <!-- footer -->
-<div class="container">
-  <footer class="py-5">
-    <div class="d-flex flex-row">
-      <div class="col-6">
-        <h5>UPT BLK Singosari</h5>
-        <div class="d-flex flex-column">
-
-          <div class="d-flex flex-row align-items-center">
-            <svg class="bi" width="24" height="24">
-              <use xlink:href="#location" />
-            </svg>
-            <div class="d-flex flex-column">
-              <p class="ms-3 mb-0">Alamat :</p>
-              <p class="ms-3 mb-0">Jl. Raya Singosari, Song Song, Ardimulyo, Kec. Singosari, Kabupaten
-                Malang, Jawa Timur, 65153</p>
-              <p class="ms-3">
-                <a target="_blank" href="https://maps.app.goo.gl/LLyo37feniFJM5fr6">Buka GMaps</a>
-              </p>
-            </div>
-          </div>
-          <div class="d-flex flex-row align-items-center">
-            <svg class="bi m-0" width="24" height="24">
-              <use xlink:href="#email" />
-            </svg>
-            <div class="d-flex flex-column">
-              <p class="ms-3 mb-0">Email :</p>
-              <p class="ms-3 mb-0">blk.singosari.jatim@gmail.com</p>
-              <p class="ms-3 mb-0">
-                <a target="_blank" href="mailto:blk.singosari.jatim@gmail.com?cc=&bcc=&subject=Informasi%20Pelatihan&body=Halo%20Admin%20UPT%20BLK%20Singosari%2C%0ASelamat%20pagi%2C%20share%20informasi%20pelatihan%20dong...%0ATerima%20kasiihhhh....">
-                  Email kami!
-                </a>
-              </p>
-            </div>
-            <div class="d-flex flex-row align-items-center">
+<footer class="py-5">
+  <div class="bg-light">
+    <div class="container">
+      <div class="d-flex flex-row">
+        <div class="col-6">
+          <div class="d-flex flex-column">
+            <h5>UPT BLK Singosari</h5>
+            <div class="d-flex flex-row align-items-center justify-content-start">
               <svg class="bi m-0" width="24" height="24">
+                <use xlink:href="#location" />
+              </svg>
+              <div class="d-flex flex-column">
+                <p class="ms-3 mb-0">Alamat : Jl. Raya Singosari, Song Song, Ardimulyo, Kec. Singosari, Kabupaten
+                  Malang, Jawa Timur, 65153</p>
+                <p class="ms-3">
+                  <button class="btn btn-primary" type="button"><a target="_blank" href="https://maps.app.goo.gl/LLyo37feniFJM5fr6" style="color: white; text-decoration: none;">Buka GMaps!</a></button>
+                </p>
+              </div>
+            </div>
+
+            <div class="d-flex flex-row align-items-center justify-content-start">
+              <svg class="bi m-0" width="16" height="16">
+                <use xlink:href="#email" />
+              </svg>
+              <div class="d-flex flex-column">
+                <p class="ms-3 m-0">Email : blk.singosari.jatim@gmail.com</p>
+                <p class="ms-3 m-0">
+                  <button class="btn btn-primary" type="button"><a target="_blank" href="mailto:blk.singosari.jatim@gmail.com?cc=&bcc=&subject=Informasi%20Pelatihan&body=Halo%20Admin%20UPT%20BLK%20Singosari%2C%0ASelamat%20pagi%2C%20share%20informasi%20pelatihan%20dong...%0ATerima%20kasiihhhh...." style="color: white; text-decoration: none;">
+                      Email kami!
+                    </a></button>
+                </p>
+              </div>
+            </div>
+
+            <div class="mt-3 d-flex flex-row align-items-center justify-content-start">
+              <svg class="bi m-0" width="16" height="16">
                 <use xlink:href="#telephone" />
               </svg>
               <div class="d-flex flex-column">
-                <p class="ms-3 mb-0">Telepon Kantor :</p>
-                <p class="ms-3 m-0">0341 - 458055</p>
-                <p class="ms-3 m-0">Whatsapp</p>
-                <p class="ms-3 m-0">082334352225</p>
+                <p class="ms-3 mb-0">Telepon Kantor : 0341 - 458055</p>
+                <p class="ms-3 m-0">Whatsapp : 082334352225</p>
                 <p class="ms-3">
-                  <a target="_blank" href="https://wa.me/6282334352225?text=Halo%20min,%20mau%20nanya%20nih!">
-                    Hubungi kami!
-                  </a>
+                  <button class="btn btn-primary" type="button"><a target="_blank" href="https://wa.me/6282334352225?text=Halo%20min,%20mau%20nanya%20nih!" style="color: white; text-decoration: none;">
+                      Hubungi kami!
+                    </a></button>
                 </p>
               </div>
             </div>
@@ -83,12 +82,12 @@
           </ul>
         </div>
       </div>
-
-      <div class="d-flex justify-content-between py-4 my-4 border-top">
-        <p>&copy; 2022 - 2024 UPT BLK Singosari.</p>
-      </div>
-  </footer>
-</div>
+    </div>
+  </div>
+  <div class="container d-flex justify-content-between py-4 my-4 border-top">
+    <p>&copy; 2022 - 2024 UPT BLK Singosari.</p>
+  </div>
+</footer>
 
 <!-- Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
