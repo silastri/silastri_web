b9709cecbda22cf54dceb7ca7a6cc39944d6c23b
Yanottama Oktabrian <oktatama4@gmail.com>
2024-07-08 11:22:13 +0700

    fix: fix button kembali ke home

--- pages/kembali.php
+++ pages/kembali.php
@@ -1,5 +1,5 @@
 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-    <a href="index.php">
+    <a href="../index.php">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
             Kembali
         </button></a>
