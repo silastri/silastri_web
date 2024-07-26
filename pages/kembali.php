566e589e48a7bce10a0f78b0b0c5256326b86d62
Yanottama Oktabrian <oktatama4@gmail.com>
2024-07-26 08:22:18 +0700

    fix: fix back button

--- pages/kembali.php
+++ pages/kembali.php
@@ -1,6 +1,12 @@
-<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+<!-- <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
     <a href="../index.php">
         <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
             Kembali
         </button></a>
+</div> -->
+
+<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+    <button type="button" class="btn btn-primary btn-lg px-4 gap-3" onclick="history.back()">
+        Kembali
+    </button>
 </div>
