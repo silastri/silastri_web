cb58b5d568f93b6df628bb4123cd39baf9d0b3af
Yanottama Oktabrian <oktatama4@gmail.com>
2024-10-08 10:19:33 +0700

    feat: update cta

--- index.php
+++ index.php
@@ -73,15 +73,12 @@
           aria-disabled="true"><button type="button" class="btn btn-danger btn-lg px-4 gap-3">
             Daftar disini!
           </button></a>
-        <a target="_blank" href="https://bit.ly/hasil-tes-online-PAP-angkatan6" style="pointer-events: none;"
-          aria-disabled="true"><button type="button" class="btn btn-danger btn-lg px-4">
-            Hasil Tes Online PAP & Pemanggilan Peserta</button></a>
         <a target="_blank" href="https://bit.ly/peserta-tes-wawancara-angkatan6" style="pointer-events: none;"
           aria-disabled="true"><button type="button" class="btn btn-danger btn-lg px-4">
             Pemanggilan Peserta Tes Offline
           </button></a>
-        <a target="_blank" href="../files/PENGUMUMAN_HASIL_TES _APBN_4_PBK_6_2024.pdf"><button type="button"
-            class="btn btn-success btn-lg px-4">
+        <a target="_blank" href="../files/PENGUMUMAN_HASIL_TES _APBN_4_PBK_6_2024.pdf" style="pointer-events: none;"
+          aria-disabled="true"><button type="button" class="btn btn-danger btn-lg px-4 gap-3">
             Pengumuman Hasil Seleksi
           </button></a>
         <a target="_blank" href="https://www.instagram.com/blk_singosari/"><button type="button"
