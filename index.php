b76430901cbdb0b8d23e30ad2d1d90a9900212d4
Yanottama Oktabrian <oktatama4@gmail.com>
2024-10-09 10:07:49 +0700

    feat: ganti kata

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
   <!-- pendaftaran -->
   <div id="pendaftaran" class="px-4 py-5 my-5 text-center">
     <img class="d-block mx-auto mb-4" src="images/pendaftaran.jpg" alt="Gambar Pendaftaran" />
-    <h2 class="display-5 fw-bold">Pelatihan Gelombang 7 2024</h2>
+    <h2 class="display-5 fw-bold">Pelatihan Angkatan 7 2024</h2>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">
         Pelatihan kerja berbasis kompetensi di UPT BLK Singosari
